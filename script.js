69bf5676dfc4660ad75ee1f0fddc49ec470f5a04
Evanny Zapata <evanny.zapata.m@gmail.com>
2026-01-16 21:11:13 -0300

    Se añaden las fases de la 4 a la 6

--- script.js
+++ script.js
@@ -63,3 +63,103 @@ function listarSospechosos() {
 
 // Ejecuta el listado inicial para verificar el sistema (por lo del break que pidieron)
 listarSospechosos();
+
+
+
+// Fase 4: El Decodificador (Métodos de String)
+
+let analizarCodigoSospechoso = (sospechoso) => {
+
+    let esPeligroso = false; // bandera para saber si el código es peligroso
+
+    // se quitan los espacios del código
+    let sinEspacios = sospechoso.codigo.trim();
+
+    // Verifico si empieza con la clave de seguridad
+    if (sinEspacios.startsWith('S3CR3T')) {
+
+        // Quitamos los primeros 6 caracteres ("S3CR3T")
+        let codigoImportante = sinEspacios.substring(6);
+
+        // Buscamos palabras clave  peligro o riesgo
+        if (
+            codigoImportante.includes('Peligro') ||
+            codigoImportante.includes('Riesgo')
+        ) {
+            esPeligroso = true;
+        }
+
+    } else {
+        return 'Correo no encriptado';
+    }
+
+    // Retornamos el resultado final
+    return esPeligroso ? 'Código de alta prioridad' : 'Código estándar';
+};
+
+
+
+    // Fase 5: El Reporte Final (Formato y Estilo)
+
+let generarReporteSeguridad = () => {
+
+    const CANTIDAD_DE_LINEAS_INFORME = 50;
+    const CANTIDAD_DE_LINEAS_TITULO = 35;
+    const ANCHO_ESPACIADOR = 12;
+
+    let lineaRepetidaSeparadora = '-'.repeat(CANTIDAD_DE_LINEAS_INFORME);
+    let lineaRepetidaSeparadoraDos = '-'.repeat(CANTIDAD_DE_LINEAS_TITULO);
+
+    console.log('\n' + lineaRepetidaSeparadora);
+    console.log('Agencia Skynet: INFORME DE SEGURIDAD');
+    console.log(lineaRepetidaSeparadora);
+
+    console.log('NOMBRE'.padEnd(ANCHO_ESPACIADOR) + ' | PELIGROSIDAD');
+    console.log(lineaRepetidaSeparadoraDos);
+
+    for (let sospechoso of baseDeDatos) {
+        let nombre = sospechoso.nombre;
+        let peligrosidad = sospechoso.nivelPeligrosidad;
+
+        console.log(`${nombre.padEnd(ANCHO_ESPACIADOR)} | ${peligrosidad}`);
+    }
+
+    console.log(lineaRepetidaSeparadoraDos + '\n');
+};
+
+
+    // Fase 6: Despliegue y Créditos
+
+    //Pruebas de los codigos 
+const sospechosoPruebaPeligroso = {
+    codigo: "   S3CR3TPeligroZonaRoja   "
+};
+
+const sospechosoPruebaSeguro = {
+    codigo: "   S3CR3TAccesoAutorizado   "
+};
+
+const sospechosoPruebaInvalido = {
+    codigo: "   ACCESO_LIBRE_123   "
+};
+
+console.log("\nPruebas del decodificador de códigos:");
+
+console.log(analizarCodigoSospechoso(sospechosoPruebaPeligroso)); // Esperado: Código de alta prioridad
+
+
+console.log(analizarCodigoSospechoso(sospechosoPruebaSeguro)); // Esperado: Código estándar
+
+
+console.log(analizarCodigoSospechoso(sospechosoPruebaInvalido)); // Esperado: Correo no encriptado
+
+
+listarSospechosos();
+generarReporteSeguridad();
+
+
+// Firma digital
+console.log("Sistema creado por los Agentes: Evanny Zapata y Simon Palacios");
+
+
+
